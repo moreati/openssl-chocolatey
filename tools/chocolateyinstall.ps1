f41ef5f86e8f09d3b8c726af1e8a3faaae7c5553
Jesse Sanford <jessesanford@gmail.com>
2016-06-29 11:35:39 -0400

    updated to mimic Openssl.Light package

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -1,36 +1,29 @@
-﻿#NOTE: Please remove any commented lines to tidy up prior to releasing the package, including this one
-# REMOVE ANYTHING BELOW THAT IS NOT NEEDED
-
-$ErrorActionPreference = 'Stop'; # stop on all errors
-
-$packageName = 'openssl' # arbitrary name for the package, used in messages
-$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url = 'https://slproweb.com/download/Win32OpenSSL-1_0_2d.exe' # download url
-$url64 = 'https://slproweb.com/download/Win64OpenSSL-1_0_2d.exe'
-
+﻿$packageId = 'OpenSSL'
+ 
+#default is to plop in c:\ -- yuck!
 $installDir = Join-Path $Env:ProgramFiles 'OpenSSL'
-
-$packageArgs = @{
-  packageName   = $packageName
-  unzipLocation = $toolsDir
-  fileType      = 'exe'
-  url           = $url
-  url64bit      = $url64
-
-  silentArgs    = '/silent', '/verysilent', '/sp-', '/suppressmsgboxes',
-                  "/DIR=`"$installDir`"";
-  validExitCodes= @(0)
+ 
+$params = @{
+  packageName = $packageId;
+  fileType = 'exe';
+  #InnoSetup - http://unattended.sourceforge.net/InnoSetup_Switches_ExitCodes.html
+  silentArgs = '/silent', '/verysilent', '/sp-', '/suppressmsgboxes',
+    "/DIR=`"$installDir`"";
+  url = 'https://slproweb.com/download/Win32OpenSSL-1_0_2h.exe'
+  url64bit = 'https://slproweb.com/download/Win64OpenSSL-1_0_2h.exe'
 }
-
-Install-ChocolateyPackage @packageArgs
-
+ 
+Install-ChocolateyPackage @params
+ 
 if (!$Env:OPENSSL_CONF)
 {
   $configPath = Join-Path $installDir 'bin\openssl.cfg'
-
+ 
   if (Test-Path $configPath)
   {
-    [Environment]::SetEnvironmentVariable('OPENSSL_CONF', $configPath, 'User')
-	Write-Host "configured OPENSSL_CONF variable as $configPath"
+    [Environment]::SetEnvironmentVariable(
+      'OPENSSL_CONF', $configPath, 'User')
+ 
+    Write-Output "Configured OPENSSL_CONF variable as $configPath"
   }
 }
